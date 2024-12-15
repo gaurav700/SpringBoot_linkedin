d1e8a625fbe21b60c3b6a94fe65812b53eef57ad
gaurav700 <jangidg0786@gmail.com>
2024-12-15 16:58:51 -0500

    adding discovery client and api gateway

---
+++ ApiGateway/src/test/java/com/LinkedIn/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LinkedIn.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
